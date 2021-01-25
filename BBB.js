956f674264641ef854d0e7c894863497a6da567f
sunshine-yckx <68091104+sunshine-yckx@users.noreply.github.com>
2021-01-25 09:51:08 +0800

    Update BBB.js

--- BBB.js
+++ BBB.js
@@ -30,7 +30,7 @@ hostname = bububao.duoshoutuan.com,
 
 const $ = new Env('步步寶')
 let notice = ''
-let CookieVal = "tokenstr":"416CA5153C606C40B311D241D527382G1611312036","Accept":"*/*","version":"10","idfa":"F44E9475-6781-4465-A350-A9B59003F3D5","Host":"bububao.duoshoutuan.com","Accept-Language":"zh-cn","platform":"2","imei":"4C54B343-5BD0-4891-AE3D-7953F81DA4F2","Content-Length":"0","User-Agent":"BBB/132 CFNetwork/1206 Darwin/20.1.0","Connection":"keep-alive","Accept-Encoding":"gzip, deflate, br","store":"appstore"
+let CookieVal = {"tokenstr":"416CA5153C606C40B311D241D527382G1611312036","Accept":"*/*","version":"10","idfa":"F44E9475-6781-4465-A350-A9B59003F3D5","Host":"bububao.duoshoutuan.com","Accept-Language":"zh-cn","platform":"2","imei":"4C54B343-5BD0-4891-AE3D-7953F81DA4F2","Content-Length":"0","User-Agent":"BBB/132 CFNetwork/1206 Darwin/20.1.0","Connection":"keep-alive","Accept-Encoding":"gzip, deflate, br","store":"appstore"}
 
 if ($.isNode()) {
       console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
